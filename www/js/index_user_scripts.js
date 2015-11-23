54296049009c8c33b9690c9844f8c5b12bc50bb5
zhaidy <dongyuzhai@gmail.com>
2015-11-23 23:09:26 +0800

    3

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -1,43 +1,39 @@
 /*jshint browser:true */
-/*global $ */(function()
-{
- "use strict";
- /*
-   hook up event handlers 
- */
- function register_event_handlers()
+/*global $ */
+(function()
  {
-    
-    
-     /* button  #btnSubmit */
-    $(document).on("click", "#btnSubmit", function(evt)
-    {
-        var server = $('#txtServer').val();
-        var playerId = $('#txtPlayerId').val();
-        GetPlayerProfile(server, playerId);
-    });
+     "use strict";
+     /*
+       hook up event handlers 
+     */
+     function register_event_handlers()
+     {
+         /* button  #btnSubmit */
+        $(document).on("click", "#btnSubmit", function(evt)
+        {
+            var server = $('#txtServer').val();
+            var playerId = $('#txtPlayerId').val();
+            GetPlayerProfile(server, playerId);
+        });
     
     }
- document.addEventListener("app.Ready", register_event_handlers, false);
+     document.addEventListener("app.Ready", register_event_handlers, false);
 })();
 
 function GetPlayerProfile(server, playerId){
-    var divToBeWorkedOn = "#AjaxPlaceHolder";
-    var webMethod = "http://naffaell.azurewebsites.net/GetPlayerService.asmx/HelloWorld";
-    var parameters = "{'server':'" + server + "','playerId':'" + playerId + "'}";
     $.ajax({
-        type: "POST",
-        url: webMethod,
-        data: '{}',
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function(msg) {
-            $(divToBeWorkedOn).html(msg.d);
-            alert("success");
+        type: "GET",
+        url: "http://naffaell.azurewebsites.net/GetPlayerService.asmx/GetPlayerProfile?server=" + server + "&playerId=" + playerId,
+        dataType: "text",
+        success: function (msg) {
+            msg = msg.replace(/\<(.*?)\>/g, "");
+            activate_page("#playerdetail");
+            var playerProfile = $.parseJSON(msg);
+            //$('#AjaxPlaceHolder').html(playerProfile.player_id + playerProfile.level + playerProfile.icon);
+            $('#imgPlayerIcon').attr("src", playerProfile.icon);
         },
-        error: function(e){
-            $(divToBeWorkedOn).html("Unavailable");
-            alert("fail");
+        error: function (e) {
+            $('#AjaxPlaceHolder').html("Unavailable");
         }
     });
 }
